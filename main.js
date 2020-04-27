dafac2de2fdf8f932135248462495ccf8021fcf2
Gio <giovanni.berl1@gmail.com>
2020-04-27 21:25:24 +0200

    aggiunto bonus

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine".
-// In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
-// Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
-// La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere.
-// BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
-// con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
-// con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
-// con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
+// il software richiede un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16)
+var diff = parseInt(prompt("inserisci livello difficolta'"));
+// generare 16 numeri casuali tra 1 e 100 se il livello e' 0
+// generare 16 numeri casuali tra 1 e 80 se il livello e' 1
+// generare 16 numeri casuali tra 1 e 50 se il livello e' 2
+if (diff == 0) {
+    var randomNums = 100;
+} else if (diff == 1) {
+    var randomNums = 80;
+} else if (diff == 2) {
+    var randomNums = 50;
+}
 
-// generare 16 numeri casuali tra 1 e 100, queste saranno le mine
-var mine = generaMine(16);
+var mine = generaMine(16, randomNums);
 console.log(`mine generate: ${mine}`);
 console.log(`in totale sono ${mine.length}`);
 
@@ -17,7 +19,9 @@ console.log(`in totale sono ${mine.length}`);
 
 var contatore = 0;
 do {
-    var numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e 100"));
+    var numeroUtente = parseInt(
+        prompt(`Inserisci un numero tra 1 e ${randomNums}`)
+    );
     contatore++;
 } while (!mine.includes(numeroUtente) && contatore < 100 - mine.length);
 
@@ -25,10 +29,10 @@ do {
 // ? contando anche la giocata andata male?
 console.log(`hai giocato ${contatore} volte prima di perdere`);
 
-function generaMine(numeroMine) {
+function generaMine(numeroMine, rNums) {
     var arr = [];
     while (arr.length < numeroMine) {
-        var numero = Math.floor(Math.random() * 100) + 1;
+        var numero = Math.floor(Math.random() * rNums) + 1;
 
         //se 'numero' non esiste nell'array allora lo pusho dentro
         if (arr.indexOf(numero) === -1) {
